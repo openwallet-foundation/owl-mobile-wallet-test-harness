58828e0a8b0c8eddb7aff6cd37950522130a0b9c
Henri Vandersleyen <hvandersleyen@duck.com>
2022-02-02 16:41:28 -0800

    its a start...not very impressive at the moment but speed will pick up.

--- aries-mobile-tests/features/steps/ODS/new_user.py
+++ aries-mobile-tests/features/steps/ODS/new_user.py
@@ -1,2 +1,10 @@
 from behave import given, when, then
+from typing import  TypeVar
 
+T = TypeVar('T')
+# None == void/null
+# reference
+# https://docs.python.org/3/library/typing.html
+@given('I am on Get started screen')
+def step_1(context:T) -> None:
+    print('strongly typed > loosely typed')
