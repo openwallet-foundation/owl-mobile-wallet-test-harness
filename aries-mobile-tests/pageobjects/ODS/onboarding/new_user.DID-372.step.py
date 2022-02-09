62adeafc6a6468236de64d852dd28c870df4fae2
Henri Vandersleyen <hvandersleyen@duck.com>
2022-02-09 10:16:55 -0800

    small chages marked by pylint

--- aries-mobile-tests/pageobjects/ODS/onboarding/new_user.DID-372.step.py
+++ aries-mobile-tests/pageobjects/ODS/onboarding/new_user.DID-372.step.py
@@ -19,18 +19,19 @@ from touchIDBiometrics import touchIDBiometrics
 
 """
 
+
 @given("I am on Get started screen")
 def stepGivenNewUser1(context) -> None:
     context.initialOnboarding = initialOnboarding(context)
-        assert (
-        context.initialOnboarding.selectGetStartedBtn() == True
+    assert (
+        context.initialOnboarding.selectGetStartedBtn() is True
     ), "We are not on the onboard screen"
 
 
 @when("I click on Check your device's security settings")
 def stepWhenNewUser1(context) -> None:
     assert (
-        context.initialOnboarding.selectSecuritySettings() == True
+        context.initialOnboarding.selectSecuritySettings() is True
     ), "Unable to navigate to the security setting page"
 
 
@@ -39,7 +40,7 @@ def stepThenNewUser1(context) -> None:
     # INFO: Weak test as I don't know what the general accessibility_id would be (think and rework)
     # link object
     assert (
-        context.initialOnboarding.selectGetStartedBtn() == False
+        context.initialOnboarding.selectGetStartedBtn() is False
     ), "We are still on the onboard screen"
 
     # multiple and are essentially a repeat of the previous1
@@ -50,35 +51,45 @@ def stepThenNewUser1(context) -> None:
   Scenario: New user reviews all explainer screens 
 
 """
+
+
 @given("My language has been set")
 def stepGivenNewUser2(context) -> None:
-    pass 
+    pass
 
 
 @given("I am on Get started screen")
 def stepGivenNewUser2And1(context) -> None:
     context.initialOnboarding = initialOnboarding(context)
-    assert context.initialOnboarding.rightPage() ==True, "You are not on the Get Started Page"
-    
+    assert (
+        context.initialOnboarding.rightPage() is True
+    ), "You are not on the Get Started Page"
+
 
 @when("I click on Get started")
 def stepWhenNewUser2(context) -> None:
-    assert context.initialOnboarding.selectGetStartedBtn() ==True, "Can't find or click the Get started Btn"
+    assert (
+        context.initialOnboarding.selectGetStartedBtn() is True
+    ), "Can't find or click the Get started Btn"
+
 
 @when("I accept Terms of service")
 def stepWhenNewUser2And1(context) -> None:
     context.termsAndConditions = termsAndConditions(context)
     context.termsAndConditions.checkTOSBox()
 
+
 @when("I click Continue on Terms of Use")
 def stepWhenNewUser2And2(context) -> None:
     context.termsAndConditions.selectContinueBtn()
 
+
 @when("I land on Store credentials screen")
 def stepWhenNewUser2And3(context) -> None:
-    #BUG: what is the store credential screen? explainerscreens?
+    # BUG: what is the store credential screen? explainerscreens?
     context.explainerPages = explainerPages(context)
     context.index = 0
+
 
 @when("I click Next")
 def stepWhenNewUser2And4(context) -> None:
@@ -90,6 +101,7 @@ def stepWhenNewUser2And4(context) -> None:
 def stepWhenNewUser2And5(context) -> None:
     pass
 
+
 @when("I click Next")
 def stepWhenNewUser2And6(context) -> None:
     context.index = context.explainerPages.selectNextBtn(context.index)
@@ -100,13 +112,14 @@ def stepWhenNewUser2And6(context) -> None:
 def stepWhenNewUser2And7(context) -> None:
     pass
 
+
 @when("I click Done")
 def stepWhenNewUser2And8(context) -> None:
     context.explainerPages.selectNextBtn(context.index)
     assert context.index == 2, "something went wrong in moving forward"
 
+
 @then("I am on Confirm your biometrics screen")
 def stepThenNewUser2(context) -> None:
     context.touchIDBiometrics = touchIDBiometrics(context)
-    assert context.touchIDBiometrics == True, "You are not on the biometric page"
-
+    assert context.touchIDBiometrics is True, "You are not on the biometric page"
