c52788573fa9394681f38326ec7f22dfe203dad5
Henri Vandersleyen <hvandersleyen@duck.com>
2022-02-10 12:23:14 -0800

    pre-merge commit

--- runTest.sh
+++ runTest.sh
@@ -8,14 +8,16 @@ then
   source .env
   # echo $DEVICE_CLOUD_USERNAME 
 else
+  echo -e '\n=> No env file found abborting operation'
   echo "Missing .env file!" > logfile.log
+  echo -e '\n=> Done'
   exit 125
 fi
 echo -e '\n=> Done'
 
-echo -e '\n=> uploading the app'
-./upload_app_to_SL.sh $DEVICE_CLOUD_USERNAME $DEVICE_CLOUD_ACCESS_KEY /$APP_APK_LOCATION "Android Bifold ON App" api.us-west-1S
-echo -e '\n=> Done'
+# echo -e '\n=> uploading the app'
+# ./upload_app_to_SL.sh $DEVICE_CLOUD_USERNAME $DEVICE_CLOUD_ACCESS_KEY /$APP_APK_LOCATION "Android Bifold ON App" api.us-west-1
+# echo -e '\n=> Done'
 
 echo -e '\n=> Removing all Dangling images'
 docker rmi "$(docker images --filter "dangling=true" -q --no-trunc)"
@@ -27,7 +29,10 @@ echo -e '\n=> Done'
 
 # testing
 echo -e '\n=> Starting the saucy tests'
-LEDGER_URL_CONFIG=http://test.bcovrin.vonx.io REGION=us-west-1 ./manage run -d SauceLabs -u $DEVICE_CLOUD_USERNAME -k $DEVICE_CLOUD_ACCESS_KEY -p Android -a app-release.apk -i acapy-main -v acapy-main -t @Connect
+LEDGER_URL_CONFIG=http://test.bcovrin.vonx.io REGION=us-west-1 ./manage run -d SauceLabs -u $DEVICE_CLOUD_USERNAME -k $DEVICE_CLOUD_ACCESS_KEY -p Android -a $APP_NAME.apk -i acapy-main -v acapy-main -t @DID-372
 echo -e '\n=> Done'
+
+#Notes:
+# regardin nehave the @is what is required to run the test. If you want to run everything use @ODS otherwise just the test
 
 
