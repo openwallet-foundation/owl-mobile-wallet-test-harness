b1cfe1c410751b404ea2efd00f2370244a89993c
Henri Vandersleyen <hvandersleyen@duck.com>
2022-02-10 14:31:18 -0800

    cleared cache and which removed the all_step.py and logfile.log from the repo (they are auto generated)

--- aries-mobile-tests/features/steps/all_steps.py
+++
@@ -1,7 +0,0 @@
-from steps.ODS.__init__ import *
-from steps.ODS.new_user_step1 import *
-from steps.ODS.new_user_step2 import *
-from steps.bc_wallet.change_language import *
-from steps.bc_wallet.connect import *
-from steps.bc_wallet.onboarding import *
-
