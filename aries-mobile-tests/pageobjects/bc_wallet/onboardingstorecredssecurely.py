6d5a7dbb562c7914d6ed63f0d9d3ffab02bb8f8f
Sheldon Regular <sheldon.regular@gmail.com>
2022-02-18 16:28:55 -0500

    updated and working 10/11 test scenarios
    Signed-off-by: Sheldon Regular <sheldon.regular@gmail.com>

--- aries-mobile-tests/pageobjects/bc_wallet/onboardingstorecredssecurely.py
+++ aries-mobile-tests/pageobjects/bc_wallet/onboardingstorecredssecurely.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pageobjects.basepage import BasePage
 from pageobjects.bc_wallet.onboardingsharenecessary import OnboardingShareNecessaryPage
+from pageobjects.bc_wallet.termsandconditions import TermsAndConditionsPage
 #from pageobjects.bc_wallet.onboardingwelcome import OnboardingWelcomePage
 
 # These classes can inherit from a BasePage to do common setup and functions
@@ -14,42 +15,43 @@ class OnboardingStoreCredsSecurelyPage(BasePage):
     # TODO: If Ontario/BC or other wallets are closely alligned and only locators are different, 
     # we could create a locator module that has all the locators. Given a specific app we could load the locators for that app. 
     # not sure this would be a use case that would be common. Leaving locators with the page objects for now.
-    title_locator = "Share your credentials securely"
-    page_text_locator = "Page Text"
-    next_locator = "Next"
+    on_this_page_text_locator = "Store and secure credentials"
+    next_locator = {
+        "iOS": "nextButtonX",
+        "Android": "Next"
+    }
+    #next_locator = "Next"
+    #next_locator = "nextButton"
     skip_locator = "Skip"
     back_locator = "Back"
 
-    def on_this_page(self):
-        if self.on_the_right_page(self.title_locator):
-            return True
-        else:
-            return False
+    def on_this_page(self):     
+        return super().on_this_page(self.on_this_page_text_locator)
 
     def get_onboarding_text(self):
-        if self.on_the_right_page(self.title_locator):
+        if self.on_this_page():
             pass
         else:
-            raise Exception(f"App not on the {self.title_locator} page")
+            raise Exception(f"App not on the {self.on_this_page_text_locator} page")
 
     def select_next(self):
-        if self.on_the_right_page(self.title_locator):
-            self.find_by_accessibility_id(self.next_locator).click()
+        if self.on_this_page():
+            self.find_by_accessibility_id(self.next_locator[self.current_platform]).click()
             return OnboardingShareNecessaryPage(self.driver)
         else:
-            raise Exception(f"App not on the {self.title_locator} page")
+            raise Exception(f"App not on the {self.on_this_page_text_locator} page")
 
     def select_back(self):
-        if self.on_the_right_page(self.title_locator):
+        if self.on_this_page():
             self.find_by_accessibility_id(self.back_locator).click()
             from pageobjects.bc_wallet.onboardingwelcome import OnboardingWelcomePage
             return OnboardingWelcomePage(self.driver)
         else:
-            raise Exception(f"App not on the {self.title_locator} page")
+            raise Exception(f"App not on the {self.on_this_page_text_locator} page")
 
     def select_skip(self):
-        if self.on_the_right_page(self.title_locator):
+        if self.on_this_page():
             self.find_by_accessibility_id(self.skip_locator).click()
-            return OnboardingShareNecessaryPage(self.driver)
+            return TermsAndConditionsPage(self.driver)
         else:
-            raise Exception(f"App not on the {self.title_locator} page")
+            raise Exception(f"App not on the {self.on_this_page_text_locator} page")
