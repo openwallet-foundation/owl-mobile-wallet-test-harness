acc3baa7c5b57c08f315e5c05daf8bfb32de40da
Anis aouadi <anis.aouadi@mcn.gouv.qc.ca>
2023-10-23 16:24:56 -0400

    enelever commentaire

--- aries-mobile-tests/features/steps/qc_wallet/qc_terms.py
+++ aries-mobile-tests/features/steps/qc_wallet/qc_terms.py
@@ -6,4 +6,3 @@ from override_steps import overrides
 @overrides('the users accepts the Terms and Conditions', 'when')
 def accept_terms_step_impl(context):
     context.thisTermsAndConditionsPage.select_accept()
-# commit
