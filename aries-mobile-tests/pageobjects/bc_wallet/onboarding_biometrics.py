f42637c39f38ace55948540f5e4d78ce19e57f56
Sheldon Regular <sheldon.regular@gmail.com>
2024-11-18 10:00:37 -0500

    Updated BC Wallet tests for new system biometrics modal
    Signed-off-by: Sheldon Regular <sheldon.regular@gmail.com>

--- aries-mobile-tests/pageobjects/bc_wallet/onboarding_biometrics.py
+++ aries-mobile-tests/pageobjects/bc_wallet/onboarding_biometrics.py
@@ -1,5 +1,6 @@
 import os
 from appium.webdriver.common.appiumby import AppiumBy
+from pageobjects.basepage import BasePage, WaitCondition
 from pageobjects.basepage import BasePage
 from pageobjects.bc_wallet.enable_notifications import EnableNotificationsPage
 
@@ -12,6 +13,10 @@ class OnboardingBiometricsPage(BasePage):
     use_biometrics_toggle_locator = (AppiumBy.ID, "com.ariesbifold:id/ToggleBiometrics")
     continue_button_locator = (AppiumBy.ID, "com.ariesbifold:id/Continue")
 
+    def __init__(self, driver):
+        super().__init__(driver)
+        # Instantiate possible Modals and Alerts for this page
+        self.enable_biometrics_system_modal = EnableBiometricsSystemModal(driver)
 
     def on_this_page(self):   
         timeout = 50
@@ -37,3 +42,28 @@ class OnboardingBiometricsPage(BasePage):
             
         else:
             raise Exception(f"App not on the {type(self)} page")
+
+
+class EnableBiometricsSystemModal(BasePage):
+    """Enable Biometrics System Modal page object"""
+
+    # Locators
+    on_this_page_text_locator = "BC Wallet wants to use your"
+    on_this_page_locator = (AppiumBy.ID, "com.ariesbifold:id/Allow")
+    allow_button_locator = (AppiumBy.ID, "com.ariesbifold:id/Allow")
+    dont_allow_button_locator = (AppiumBy.ID, "com.ariesbifold:id/DontAllow")
+
+    def on_this_page(self):    
+        return super().on_this_page(self.on_this_page_locator) 
+
+    def select_allow(self):
+        self.find_by(self.allow_button_locator, wait_condition=WaitCondition.PRESENCE_OF_ELEMENT_LOCATED).click()
+        return OnboardingBiometricsPage(self.driver)
+    
+        # if self.driver.capabilities['platformName'] == 'Android':
+        #     self.select_system_allow_while_using_app()
+        # return True
+
+    def select_dont_allow(self):
+        self.find_by(self.dont_allow_button_locator, wait_condition=WaitCondition.PRESENCE_OF_ELEMENT_LOCATED).click()
+        return OnboardingBiometricsPage(self.driver)
